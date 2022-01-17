fc5234d1ab9037161710f3032762a653fb15542f
Philrobots <47373969+Philrobots@users.noreply.github.com>
2022-01-17 11:25:07 -0500

    feat(3):first-api-call (#9)
    * feat(3):first-api-call
    
    * feat(3):add-test

--- src/main/java/ulaval/glo2003/ApplicationMain.java
+++ src/main/java/ulaval/glo2003/ApplicationMain.java
@@ -3,16 +3,21 @@ package ulaval.glo2003;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import ulaval.glo2003.health.api.HealthResource;
 
 import java.io.IOException;
 import java.net.URI;
 
-public class Main {
+public class ApplicationMain {
+
+    private static final String URL = "http://localhost:8080/";
+    private static final String PACKAGE = "ulaval.glo2003";
 
     public static void main(String[] args) throws IOException {
+        HealthResource healthResource = new HealthResource();
         ResourceConfig resourceConfig = new ResourceConfig()
-                .packages("ulaval.glo2003");
-        URI uri = URI.create("http://localhost:8080/");
+                .packages(PACKAGE).register(healthResource);
+        URI uri = URI.create(URL);
 
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
         server.start();
---
+++ src/test/java/ulaval/glo2003/health/api/HealthResourceTest.java
@@ -0,0 +1,22 @@
+package ulaval.glo2003.health.api;
+
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class HealthResourceTest {
+
+    private HealthResource healthResource = new HealthResource();
+
+    @Test
+    public void whenGetHealth_thenShouldReturnOkStatus() {
+        int expectedStatus = 200;
+
+        Response actualResponse = healthResource.getHealth();
+
+        assertEquals(expectedStatus, actualResponse.getStatus());
+    }
+
+}
